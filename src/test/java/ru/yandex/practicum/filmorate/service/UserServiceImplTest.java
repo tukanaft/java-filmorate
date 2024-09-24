f7df0f46a0417b665c1512f421a84377e51cf659
tukanaft <tukanaft@gmail.com>
2024-09-24 16:34:46 +0300

    init

--- src/test/java/ru/yandex/practicum/filmorate/service/UserServiceImplTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/service/UserServiceImplTest.java
@@ -8,7 +8,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.User;
 
-import java.lang.reflect.Array;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Arrays;
