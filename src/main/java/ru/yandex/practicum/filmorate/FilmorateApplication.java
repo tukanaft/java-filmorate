5a866a634b68f119b88837ca87579f9e659df8c1
tukanaft <tukanaft@gmail.com>
2024-09-28 17:42:25 +0300

    init

--- src/main/java/ru/yandex/practicum/filmorate/FilmorateApplication.java
+++ src/main/java/ru/yandex/practicum/filmorate/FilmorateApplication.java
@@ -1,10 +1,8 @@
 package ru.yandex.practicum.filmorate;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import ru.yandex.practicum.filmorate.controller.FilmCreation;
 
 @SpringBootApplication
 public class FilmorateApplication implements CommandLineRunner {
